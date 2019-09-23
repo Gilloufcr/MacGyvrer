c3b5f0610de9de95a2a4399787d742211704f5fc
gilloufcr <Gilles@macbook-pro-de-gilles.home>
2019-09-23 16:16:21 +0200

    suite predicat and or

--- cours.py
+++ cours.py
@@ -1,10 +1,25 @@
 #Tests de conditions
-mon_age = 5
-if mon_age > 20:
-    print("Tu as bien grandi!")
-elif mon_age >= 16:
-    print("et tu est meme presque majeur")
-    if mon_age == 17:
-        print("Well done!!")
-else:
-    print("Okayyyy")
+#mon_age = 5
+#if mon_age > 20:
+#   print("Tu as bien grandi!")
+#elif mon_age >= 16:
+#   print("et tu est meme presque majeur")
+#   if mon_age == 17:
+#       print("Well done!!")
+#else:
+#   print("Okayyyy")
+
+#Test de predicat
+#age = 20
+#majeur = False
+#if age >= 18:
+#   majeur == True
+
+#Test and or not
+
+#age = 19
+
+#if age < 18 or age > 21:
+#   print("age ok")
+#else:
+#    print("not at all")
