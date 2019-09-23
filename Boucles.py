2d1ea3fd5cdefdd856cf0861c9d855c91eaa7fe7
gilloufcr <Gilles@macbook-pro-de-gilles.home>
2019-09-23 22:25:40 +0200

    Boucles OK

--- Boucles.py
+++ Boucles.py
@@ -2,11 +2,20 @@
 
 #Test de Boucle While table de multiplication
 
-tbl = input("Merci de saisir la table que vous souhaitez multiplier :")
+#tbl = input("Merci de saisir la table que vous souhaitez multiplier :")
 
-i = 0
+#i = 0
 
-while i <= 10:
-    result = int(tbl) * int(i)
-    print(result)
-    i += 1
+#while i <= 10:
+#    result = int(tbl) * int(i)
+#    print(result)
+#    i += 1
+
+#Boucle for
+
+chaine = "Bonjour les ZER0"
+for lettre in chaine:
+    if lettre in "AEIOUYaeiouy":
+        print(lettre)
+    else:
+        print("*")
