fcbe4b1d31b9ed599685d6df8d67a4029007516e
gilloufcr <Gilles@macbook-pro-de-gilles.home>
2019-09-23 15:07:26 +0200

    Conditions

---
+++ cours.py
@@ -0,0 +1,10 @@
+#Tests de conditions
+mon_age = 5
+if mon_age > 20:
+    print("Tu as bien grandi!")
+elif mon_age >= 16:
+    print("et tu est meme presque majeur")
+    if mon_age == 17:
+        print("Well done!!")
+else:
+    print("Okayyyy")
