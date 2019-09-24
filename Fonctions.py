c6c428b28e6c081d5562224f74d315d4c9a5c5fa
gilloufcr <Gilles@macbook-pro-de-gilles.home>
2019-09-24 10:30:49 +0200

    Cours modules

---
+++ Fonctions.py
@@ -0,0 +1,10 @@
+# -*-coding:Utf-8 -*
+"""module multipli contenant la fonction table"""
+
+def table_multi(nb, max):
+ 
+    i = 0
+    while i < max:
+        result = int(nb) * int(i)
+        print(result)
+        i += 1
---
+++ test.py
@@ -0,0 +1,7 @@
+# -*-coding:Utf-8 -*
+'''Test du fichier Fonction.py'''
+
+import os
+from Fonctions import *
+
+table_multi(5, 20)
