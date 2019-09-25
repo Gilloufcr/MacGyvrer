0897b6eb65ef381f84cea8736d2c35c651156904
gilloufcr <Gilles@macbook-pro-de-gilles.home>
2019-09-25 13:28:09 +0200

    Version de vincent ajoutée a la mienne

--- ZCasino/ZCasino.py
+++ ZCasino/ZCasino.py
@@ -1,28 +1,70 @@
-import math
-import random
+# -*-coding:Utf-8 -*
 
-mise = 0
-nummise = 0
-numroul = 0
-gain = 0
+from math import ceil
+from random import randrange
 
-nummise = input ("Merci de Saisir un Numéro entre 0 et 49: ")
-mise = input("Merci d'indiquer votre mise $ : ")
-numroul = random.randrange(0,49)
+argent = 1000
+continuer_partie = True
 
-#while numroul < 0:
+print("Vous vous installez à la table de roulette avec", argent, "$.")
 
-nummise = int(nummise)
-mise = int(mise)
+while continuer_partie == True:
+    nummise = -1
+    #Choix du numéro
+    while nummise < 0 or nummise > 49:
+        nummise = input("Merci de Saisir un Numéro entre 0 et 49: ")
+        try:
+            nummise = int(nummise)
+        except ValueError:
+            print("Le numéro Saisi n'est pas correct")
+            nummise = -1
+            continue
 
-if nummise == numroul:
-    gain = int(mise) * 3
-    print("Le numéro de la Banque est :", numroul)
-    print("Bravo! vous avez gagné ", gain + mise, "$ !!")
-elif (nummise != numroul and nummise % 2 == 0 and numroul %2 == 0) or (nummise != numroul and numroul % 2 != 0 and nummise % 2 != 0):
-    gain = int(mise) * 0.5
-    print("Le numéro de la Banque est :", numroul)
-    print("Dommage ce n'est pas le bon numéro :( mais c'est la bonne couleur ! Vous gagnez ", math.ceil(gain) + mise, "$ !!")
-elif nummise != numroul and int(numroul) % 2 != 0:
-    print("Le numéro de la Banque est :", numroul)
-    print("Désolé vous avez Perdu :( :(")
+    #Choix de la mise
+    
+    mise = 0
+    while mise <= 0 or mise > argent:
+        mise = input("Merci d'indiquer votre mise $ : ")
+        try:
+            mise = int(mise)
+        except ValueError:
+            print("Le numéro Saisi n'est pas correct")
+            mise = -1
+            continue
+        if mise > argent:
+            print("Vous avez misé plus que votre argent..")
+
+    # La mise est selectionnée et le numéro choisi, on lance la roulette.
+
+    numroul = randrange(50)
+
+    if nummise == numroul:
+        gain = int(mise) * 3
+        print("Le numéro de la Banque est :", numroul)
+        print("Bravo! vous avez gagné ", gain + mise, "$ !!")
+        argent = argent + gain + mise
+        print("votre argent est de ", argent, "$!")
+
+    elif (nummise != numroul and nummise % 2 == 0 and numroul %2 == 0) or (nummise != numroul and numroul % 2 != 0 and nummise % 2 != 0):
+        gain = int(mise) * 0.5
+        print("Le numéro de la Banque est :", numroul)
+        print("Dommage ce n'est pas le bon numéro :( mais c'est la bonne couleur ! Vous gagnez ", ceil(gain) + mise, "$ !!")
+        argent = argent + gain + mise
+        print("votre argent est de ", argent, "$!")
+
+    else:
+        print("Le numéro de la Banque est :", numroul)
+        print("Désolé vous avez Perdu :( :(")
+        argent = argent - mise
+        print("votre argent est de ", argent, "$!")
+    
+    if argent <= 0:
+        print(" Désolé vous n'avez plus d'argent")
+        continuer_partie = False
+    else:
+        quitter = input("Souhaitez vous quitter le jeux ? o/n :")
+        if quitter == "o" or quitter == "O":
+            print("Vous avez Gagné ", argent,"$ !!")
+            continuer_partie = False
+
+    
