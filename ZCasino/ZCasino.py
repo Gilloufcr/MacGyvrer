7dfb3b75902765c8781b7d28873920c1b3a243bc
gilloufcr <Gilles@macbook-pro-de-gilles.home>
2019-09-25 12:18:39 +0200

    correction fonction math.ceil

--- ZCasino/ZCasino.py
+++ ZCasino/ZCasino.py
@@ -13,15 +13,16 @@ numroul = random.randrange(0,49)
 #while numroul < 0:
 
 nummise = int(nummise)
+mise = int(mise)
 
 if nummise == numroul:
     gain = int(mise) * 3
     print("Le numéro de la Banque est :", numroul)
-    print("Bravo! vous avez gagné ", gain, "$ !!")
+    print("Bravo! vous avez gagné ", gain + mise, "$ !!")
 elif (nummise != numroul and nummise % 2 == 0 and numroul %2 == 0) or (nummise != numroul and numroul % 2 != 0 and nummise % 2 != 0):
     gain = int(mise) * 0.5
     print("Le numéro de la Banque est :", numroul)
-    print("Dommage ce n'est pas le bon numéro :( mais c'est la bonne couleur ! Vous gagnez ", math.ceil(int(gain)), "$ !!")
+    print("Dommage ce n'est pas le bon numéro :( mais c'est la bonne couleur ! Vous gagnez ", math.ceil(gain) + mise, "$ !!")
 elif nummise != numroul and int(numroul) % 2 != 0:
     print("Le numéro de la Banque est :", numroul)
     print("Désolé vous avez Perdu :( :(")
