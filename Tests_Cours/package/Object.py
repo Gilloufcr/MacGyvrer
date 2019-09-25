c0c0966f3c18a406576a5fdec9252e1cc7e13910
gilloufcr <Gilles@macbook-pro-de-gilles.home>
2019-09-25 16:03:36 +0200

    passage au listes

---
+++ Tests_Cours/package/Object.py
@@ -0,0 +1,21 @@
+
+'''machaine = str()
+
+while machaine.lower() != "q":
+    print("taper Q pour quitter")
+    machaine = input()
+
+print("Merci!")'''
+
+'''prenom = input("Prenom :")
+nom = input("nom :")
+age = input("age :")
+
+print("Je m'appelle {0} {1} ({3} {0} pour l'administration) et j'ai {2} ans.".format(prenom, nom, age, nom.upper()))'''
+
+'''machaine = "Bonjour"
+i = 0
+
+while i < len(machaine):
+    print(machaine[i])
+    i += 1'''
