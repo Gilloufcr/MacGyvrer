928186be85ad652d4e59973738e9fd0a512bf6b6
gilloufcr <Gilles@macbook-pro-de-gilles.home>
2019-10-01 16:18:03 +0200

    test fichiers

--- Test_ecriture_lecture_file/fichiers.py
+++ Test_ecriture_lecture_file/fichiers.py
@@ -1,5 +1,20 @@
 # -*-coding:Utf-8 -*
+import pickle
 
-lecture_fichier = open("fichier.txt", "r")
+'''lecture_fichier = open("fichier.txt", "r")
+#contenu = lecture_fichier.write("Premier Test d'ecriture")
+contenu = lecture_fichier.read()
 lecture_fichier.close()
-print(lecture_fichier)
+print(contenu)
+'''
+
+'''score = {"toto":10, "titi":9, "tutu": 15}
+
+with open('donnee', 'wb') as fichier:
+    mon_pickler = pickle.Pickler(fichier)
+    mon_pickler.dump(score)
+'''
+
+with open('donnee', 'rb') as fichier:
+    mon_pickler = pickle.Unpickler(fichier)
+    print(fichier)
