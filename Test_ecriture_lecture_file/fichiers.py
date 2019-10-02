8f309857a605a0416948e61a35abd167c9a13200
gilloufcr <Gilles@macbook-pro-de-gilles.home>
2019-10-02 19:24:39 +0200

    Fichiers suite

--- Test_ecriture_lecture_file/fichiers.py
+++ Test_ecriture_lecture_file/fichiers.py
@@ -15,6 +15,30 @@ with open('donnee', 'wb') as fichier:
     mon_pickler.dump(score)
 '''
 
-with open('donnee', 'rb') as fichier:
+'''with open('donnee', 'rb') as fichier:
     mon_pickler = pickle.Unpickler(fichier)
-    print(fichier)
+    score = mon_pickler.load()
+    print(score)
+'''
+
+'''def set_var(nouvelle_valeur):
+    try:
+        print("La valleur de var est {0}".format(var))
+    except NameError:
+        print("La valeur de var n'est pas connue.....")
+    var = nouvelle_valeur
+    print("Apres l'affectation var vaut : {0}".format(var))
+
+if __name__ == "__main__":
+    set_var(10)
+'''
+
+
+def ajouter_liste(ma_liste, values):
+    ma_liste.append(values)
+    
+    print(liste)
+
+if __name__ == "__main__":
+    liste = ['a', 'b', 'c']
+    ajouter_liste(liste, 'e')
